e06b98a8d0663305f5adc35976b6e5c03c0a40bc
Briet Sparks <brietsparks@gmail.com>
2022-06-25 15:23:37 -0500

    stuff

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,10 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+import http from 'http';
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
+import { hello } from './hello';
 
-// Below are examples of using ESLint errors suppression
-// Here it is suppressing a missing return type definition for the greeter function.
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
-}
+http.createServer(function (req, res) {
+  res.write(hello('world!')); //write a response
+  res.end();
+}).listen(3000, function(){
+  console.log("server start at port 3000");
+});
