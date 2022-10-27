5344103b30682dd442ec36660cd865924e539692
Briet Sparks <brietsparks@gmail.com>
2022-10-27 16:48:46 -0500

    TransactionsHelper test

---
+++ service/src/__tests__/repositories/transactions.test.ts
@@ -0,0 +1,118 @@
+import { StartedTestContainer } from 'testcontainers';
+import { Knex } from 'knex';
+import { v4 as uuid } from 'uuid';
+
+import { createPgTestcontainer, makeKnexClient } from '../../database';
+import { TransactionsHelper } from '../../repositories/transactions';
+
+describe('TransactionsHelper', () => {
+  let testcontainer: StartedTestContainer;
+  let db: Knex;
+
+  beforeAll(async () => {
+    const result = await createPgTestcontainer();
+    testcontainer = result.testcontainer;
+    db = await makeKnexClient({
+      connection: result.params
+    });
+
+    await db.raw(`
+      create table lists
+      (
+        id uuid primary key
+      );
+
+      create table items
+      (
+        id      uuid primary key,
+        list_id uuid references lists (id)
+      );
+    `);
+  });
+
+  afterAll(async () => {
+    await testcontainer.stop();
+    await db.destroy();
+  })
+
+  it('commits a clean transaction', async () => {
+    const listTrx = new TransactionsHelper(db);
+    const itemTrx = new TransactionsHelper(db);
+
+    const itemId = uuid();
+    const listId = uuid();
+
+    const result = await itemTrx.query({ commit: true }, async (t1) => {
+      const list = await listTrx.query( { trx: t1, commit: false },  async (t2) => {
+        const list = { id: listId };
+        await t2.into('lists').insert(list);
+        return list;
+      });
+
+      const item = {
+        id: itemId,
+        list_id: list.id
+      };
+      await t1.into('items').insert(item);
+
+      return item;
+    });
+
+    expect(result).toEqual({
+      id: itemId,
+      list_id: listId
+    });
+
+    const retrievedItem = await db
+      .from('items')
+      .select('*')
+      .where({ 'id': itemId })
+      .first();
+
+    expect(retrievedItem).toEqual({
+      id: itemId,
+      list_id: listId
+    });
+
+    const retrievedList = await db
+      .from('lists')
+      .select('*')
+      .where({ 'id': listId })
+      .first();
+
+    expect(retrievedList).toEqual({
+      id: listId
+    });
+  });
+
+  it('rolls back a failed transaction', async () => {
+    const listTrx = new TransactionsHelper(db);
+    const itemTrx = new TransactionsHelper(db);
+
+    const itemId = uuid();
+    const error = new Error();
+
+    const promise = itemTrx.query({ commit: true }, async (t1) => {
+      await listTrx.query( { trx: t1, commit: false },  async () => {
+        throw error;
+      });
+
+      const item = {
+        id: itemId,
+      };
+      await t1.into('items').insert(item);
+
+      return item;
+    });
+
+    await expect(promise).rejects.toThrowError(error);
+
+    const retrievedItem = await db
+      .from('items')
+      .select('*')
+      .where({ 'id': itemId })
+      .first();
+
+    expect(retrievedItem).toBeUndefined();
+  });
+});
